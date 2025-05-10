753acae97dbc34aa53769b8b086ab379fa33e27c
Parkourer10 <parkourer10@gmail.com>
2025-05-10 12:01:44 +0530

    e

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+import nextra from 'nextra'
+ 
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+})
+ 
+// Export the final Next.js config with Nextra included
+export default withNextra({
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/get-started',
+        permanent: true,
+      },
+      {
+        source: '/resources',
+        destination: '/get-started',
+        permanent: true,
+      },
+    ]
+  }
+})
